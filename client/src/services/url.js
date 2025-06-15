8ca2af002959f0ac0bb9ae3ce7db5b2e943d7378
ALICE BERTHELOT <aliceberthelot.pro@gmail.com>
2025-06-15 15:37:36 +0200

    reset to remove framework

--- client/src/services/url.js
+++
@@ -1,9 +0,0 @@
-// URL paths to be appended to the main URL for HTTP requests
-
-export const baseRegisterUrl = "/api/auth/users";
-export const baseLoginUrl = "/api/auth/login";
-export const baseArtUrl = "/api/arts/";
-export const baseUserUrl = "/api/users/";
-export const basePictureUrl = "/api/pictures/";
-export const baseUploadUrl = "/api/upload/";
-export const baseAcceptedArtUrl = "/api/arts/accepted";
